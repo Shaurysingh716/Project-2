1d45f225fbadc6770e76cd95159a55615e633ef0
Shaurysingh716 <107102096+Shaurysingh716@users.noreply.github.com>
2023-06-07 17:37:49 +0530

    Add files via upload

---
+++ modals_script.js
@@ -0,0 +1,9 @@
+function ShowCourses(){
+    document.querySelector('.courses-dropdown').classList.add('show-courses-dropdown')
+}
+function ShowSignInModal(){
+    document.querySelector('.Log-In-Modal').classList.add('Show-Log-In-Modal')
+}
+function RemoveDropDown(){
+    document.querySelector('.courses-dropdown').classList.remove('show-courses-dropdown')
+}
